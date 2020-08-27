556fa64709372cd3c72c8e62d24d075bc2820c9b
Educorreia932 <skelozard1@gmail.com>
2020-08-27 03:35:15 +0100

    Drawing the points around the circle

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
+#include <cmath>
 #include <SFML/Graphics.hpp>
 
 #define WIDTH 800
 #define HEIGHT 800
 
+#define PI 3.14159265
+
 using namespace std;
+
+sf::Vector2f* generatePoints(int n, float radius) {
+	sf::Vector2f* points = new sf::Vector2f[n];
+	const float divisionAngle = 2 * PI / n;
+
+	for (int i = 0; i < n; i++) {
+		float currentAngle = divisionAngle * i;
+
+		points[i] = sf::Vector2f(cos(currentAngle) * radius + 400, sin(currentAngle) * radius + 400);
+	}
+
+	return points;
+}
 
 int main() {
 	sf::ContextSettings settings;
@@ -12,13 +28,16 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "String Art", sf::Style::Default, settings);
 	
 	sf::CircleShape circle(350);
-	circle.setFillColor(sf::Color::White);
+	circle.setFillColor(sf::Color::Red);
 	circle.setPointCount(100);
+	circle.setPosition(400.f - circle.getRadius(), 400.f - circle.getRadius());
 
-	sf::Vertex line[] = {
-		sf::Vertex(sf::Vector2f(10, 10)),
-		sf::Vertex(sf::Vector2f(150, 150))
-	};
+	sf::Vector2f* points = generatePoints(100, 350);
+
+	//sf::Vertex line[] = {
+	//	sf::Vertex(sf::Vector2f(10, 10)),
+	//	sf::Vertex(sf::Vector2f(150, 150))
+	//};
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -29,9 +48,18 @@ int main() {
 
 		window.clear();
 		window.draw(circle);
-		window.draw(line, 2, sf::Lines);
+		
+		for (int i = 0; i < 100; i++) {
+			sf::CircleShape point(5);
+			point.setFillColor(sf::Color::White);
+			point.setPosition(points[i].x - point.getRadius(), points[i].y - point.getRadius()) ;
+			window.draw(point);
+		}
+
+		//window.draw(line, 2, sf::Lines);
 		window.display();
 	}
 
 	return 0;
 }
+
