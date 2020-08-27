9af97b70b29a33cdea1b3a2a31418ac02d3d8a5e
Educorreia932 <skelozard1@gmail.com>
2020-08-27 17:48:42 +0100

    Drawing simple black and white images

--- main.cpp
+++ main.cpp
@@ -10,9 +10,55 @@
 
 using namespace std;
 
+vector<sf::Vector2f> getPixelsInLine(sf::Vector2f& startCoordinates, sf::Vector2f& endCoordinates) {
+	bool steep = false;
+	float x0 = startCoordinates.x;
+	float y0 = startCoordinates.y;
+	float x1 = endCoordinates.x;
+	float y1 = endCoordinates.y;
+
+	vector<sf::Vector2f> pixels;
+
+	if (abs(x0 - x1) < abs(y0 - y1)) {
+		swap(x0, y0);
+		swap(x1, y1);
+		steep = true;
+	}
+
+	if (x0 > x1) {
+		swap(x0, x1);
+		swap(y0, y1);
+	}
+
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int derror2 = std::abs(dy) * 2;
+	int error2 = 0;
+	int y = y0;
+
+	for (int x = x0; x <= x1; x++) {
+		if (steep) {
+			pixels.push_back(sf::Vector2f(y, x));
+		}
+
+		else {
+			pixels.push_back(sf::Vector2f(x, y));
+		}
+
+		error2 += derror2;
+
+		if (error2 > dx) {
+			y += (y1 > y0 ? 1 : -1);
+			error2 -= dx * 2;
+		}
+	}
+
+	return pixels;
+}
+
 sf::Vector2f* generatePoints(int n, float radius) {
 	sf::Vector2f* points = new sf::Vector2f[n];
-	const float divisionAngle = 2 * PI / n;
+	const double divisionAngle = 2 * PI / n;
 
 	for (int i = 0; i < n; i++) {
 		float currentAngle = divisionAngle * i;
@@ -21,6 +67,49 @@ sf::Vector2f* generatePoints(int n, float radius) {
 	}
 
 	return points;
+}
+
+vector<pair<int, int>> generateLines(sf::Vector2f* &points, sf::Image &image) {
+	vector<pair<int, int>> lines;
+
+	int choosenPointIndex = 0;
+	
+	vector<sf::Vector2f> pixelsToErase;
+
+	// Number of strings
+	for (int i = 0; i < 200; i++) {
+		int startPointIndex = choosenPointIndex;
+		float bestScore = 0;
+
+		// Iterate over every point to see where to connect the string to
+		for (int j = 0; j < 100; j++) {
+			vector<sf::Vector2f> pixels = getPixelsInLine(points[startPointIndex], points[j]);
+			float score = 0;
+
+			for (sf::Vector2f pixel : pixels) {
+				sf::Color color = image.getPixel(pixel.x, pixel.y);
+
+				if (0.3 * color.r + 0.59 * color.g + 0.11 * color.r < 200)
+					score += 1;
+			}
+
+			if (score > bestScore && j != choosenPointIndex) {
+				choosenPointIndex = j;
+				bestScore = score;
+				pixelsToErase = pixels;
+			}
+		}
+
+		int endPointIndex = choosenPointIndex;
+		pair<int, int> line = {startPointIndex, endPointIndex};
+
+		lines.push_back(line);
+
+		for (sf::Vector2f pixel : pixelsToErase)
+			image.setPixel(pixel.x, pixel.y, sf::Color::White);
+	}
+
+	return lines;
 }
 
 void drawPoint(sf::RenderWindow &window, sf::Vector2f &coordinates) {
@@ -32,72 +121,11 @@ void drawPoint(sf::RenderWindow &window, sf::Vector2f &coordinates) {
 
 void drawLine(sf::RenderWindow &window, sf::Vector2f &startCoordinates, sf::Vector2f &endCoordinates) {
 	sf::Vertex line[] = {
-		sf::Vertex(startCoordinates),
-		sf::Vertex(endCoordinates)
+		sf::Vertex(startCoordinates, sf::Color::Red),
+		sf::Vertex(endCoordinates, sf::Color::Red)
 	};
 
 	window.draw(line, 2, sf::Lines);
-}
-
-void drawImage(sf::RenderWindow& window) {
-	sf::Image image;
-	
-	if (!image.loadFromFile("dog.jpg"))
-		cerr << "Couldn't read image";
-
-	sf::Texture texture;
-	texture.loadFromImage(image);  //Load Texture from image
-
-	sf::Sprite sprite;
-	sprite.setTexture(texture);
-
-	window.draw(sprite);
-}
-
-vector<sf::Vector2f> getPixelsInLine(sf::Vector2f& startCoordinates, sf::Vector2f& endCoordinates) {
-	bool steep = false;
-	float x0 = startCoordinates.x;
-	float y0 = startCoordinates.y;
-	float x1 = endCoordinates.x;
-	float y1 = endCoordinates.y;
-
-	vector<sf::Vector2f> pixels;
-	
-	if (abs(x0 - x1) < abs(y0 - y1)) {
-		swap(x0, y0);
-		swap(x1, y1);
-		steep = true;
-	}
-
-	if (x0 > x1) {
-		swap(x0, x1);
-		swap(y0, y1);
-	}
-	
-	int dx = x1 - x0;
-	int dy = y1 - y0;
-	int derror2 = std::abs(dy) * 2;
-	int error2 = 0;
-	int y = y0;
-	
-	for (int x = x0; x <= x1; x++) {
-		if (steep) {
-			pixels.push_back(sf::Vector2f(y, x));
-		}
-
-		else {
-			pixels.push_back(sf::Vector2f(x, y));
-		}
-		
-		error2 += derror2;
-		
-		if (error2 > dx) {
-			y += (y1 > y0 ? 1 : -1);
-			error2 -= dx * 2;
-		}
-	}
-
-	return pixels;
 }
 
 int main() {
@@ -115,8 +143,13 @@ int main() {
 
 	sf::Image image;
 
-	if (!image.loadFromFile("dog.jpg"))
+	if (!image.loadFromFile("A.jpg")) {
 		cerr << "Couldn't read image";
+		
+		return 0;
+	}
+
+	vector<pair<int, int>> lines = generateLines(points, image);
 
 	sf::Texture texture;
 	texture.loadFromImage(image);  //Load Texture from image
@@ -127,30 +160,18 @@ int main() {
 	while (window.isOpen()) {
 		sf::Event event;
 
-		while (window.pollEvent(event)) 
+		while (window.pollEvent(event))
 			if (event.type == sf::Event::Closed)
 				window.close();
 
 		window.clear();
-		window.draw(circle);
 		window.draw(sprite);
-		
+
 		for (int i = 0; i < 100; i++)
 			drawPoint(window, points[i]);
 
-		int choosenPoint = 0;
-
-		// Number of strings
-		for (int i = 0; i < 10; i++) {
-			// Iterate over every point to see where to connect the string to
-			for (int j = 0; j < 100; j++) {
-				vector<sf::Vector2f> pixels = getPixelsInLine(points[choosenPoint], points[j]);
-				
-				for (sf::Vector2f pixel : pixels) {
-					image.getPixel(pixel.x, pixel.y).toInteger();
-				}
-			}
-		}
+		for (int i = 0; i < lines.size(); i++)
+			drawLine(window, points[lines[i].first], points[lines[i].second]);
 
 		window.display();
 	}
